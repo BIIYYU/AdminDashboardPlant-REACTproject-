74ddd5648d08f693254a933cfa127335189ecb51
BIIYYU <abiyyuaqils@gmail.com>
2023-07-11 10:36:18 +0700

    Perbaikan Tampilan

--- src/Components/Body Section/Top Section/Top.jsx
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -6,6 +6,7 @@ import { TbMessageCircle2 } from 'react-icons/tb';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 
 import img from '../../../Assets/user.jpg'
+import video from '../../../Assets/video.mp4'
 
 const Top = () => {
     return (
@@ -23,10 +24,26 @@ const Top = () => {
                     <TbMessageCircle2 className='icon'/>
                     <IoMdNotificationsOutline className='icon'/>
                     <div className="adminImage">
-                          {/* <img src={img} alt="Admin Image" /> */}
+                          <img src={img} alt="Admin Image" />
                     </div>
                 </div>
             </div>
+            <div className="cardSection flex">
+                <div className="rightCard flex">
+                    <h1>Create and sell extraordiary products</h1>
+                    <p>The world's fast growing industry today are natural made poducts!</p>
+                    <div className="buttons flex">
+                        <button className="btn">Explore More</button>
+                        <button className="btn transparent">Top Sellers</button>
+                    </div>
+
+                    <div className="videoDiv">
+                        <video src={video} autoPlay loop muted></video>
+                    </div>
+                </div>
+
+            </div>
+           
         </div>
     )
 }
